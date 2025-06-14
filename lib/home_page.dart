34c6a16d3aa8360e4ec12231c73962966f777cdc
Marwan Mahmoud <marwan.mh211@gmail.com>
2025-06-14 21:28:46 +0300

    Bottom sheet added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:taska/home_page_components/appbar_titles.dart';
+import 'package:taska/home_page_components/bottom_sheet.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,7 +14,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            builder: (context) => Padding(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: SingleChildScrollView(
+                child: Container(
+                  height: screenHeight * 0.18,
+                  padding: const EdgeInsets.all(16),
+                  child: const BottomSheetComponents(),
+                ),
+              ),
+            ),
+          );
+        },
+        backgroundColor: const Color(0xff55949b),
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
       drawer: const Drawer(),
       appBar: AppBar(
         iconTheme: const IconThemeData(size: 32),
---
+++ lib/home_page_components/bottom_sheet.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetComponents extends StatefulWidget {
+  const BottomSheetComponents({super.key});
+
+  @override
+  State<BottomSheetComponents> createState() => _BottomSheetComponentsState();
+}
+
+class _BottomSheetComponentsState extends State<BottomSheetComponents> {
+  bool isTextNotEmpty = false;
+  late TextEditingController name;
+  GlobalKey<FormState> formState = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+    name = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    name.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Column(
+      children: [
+        Form(
+          key: formState,
+          child: TextFormField(
+            onChanged: (value) {
+              setState(() {
+                isTextNotEmpty = value.trim().isNotEmpty;
+              });
+            },
+            autofocus: true,
+            controller: name,
+            decoration: const InputDecoration(
+              hint: Text(
+                "New Task",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              border: UnderlineInputBorder(borderSide: BorderSide.none),
+            ),
+          ),
+        ),
+        SizedBox(height: screenHeight * 0.017),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            isTextNotEmpty == true
+                ? InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "Save",
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenWidth * 0.04,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                : Text(
+                    "Save",
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: screenWidth * 0.04,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+          ],
+        ),
+      ],
+    );
+  }
+}
